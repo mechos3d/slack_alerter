7a57ca57c091b7df8d795050059e2019c8e0cecd
Gorgolevskiy Pavel <gorgolevsky@gmail.com>
2021-10-21 23:59:18 +0300

    add stuff

--- all.rb
+++ all.rb
@@ -1,1 +1,31 @@
+require 'pry'
+
+class A
+
+  def good
+    '111'
+  end
+
+  private
+
+  def bad
+    '111'
+  end
+end
+
+class B
+  def call
+    a = A.instance_methods - A.ancestors[1..-1].flat_map { |x| x.instance_methods }.uniq
+    # => [:good]
+    b = A.private_instance_methods - A.ancestors[1..-1].flat_map { |x| x.private_instance_methods }.uniq
+    # => [:bad]
+    #
+    # A.protected_instance_methods # TODO: - этих тоже надо учесть !
+    # NOTE: при таком подходе не будут учтены методы, которые объявлены в самом A, но тоже объявлены выше в иерархии..
+    binding.pry
+  end
+end
+
 # A.instance_methods - A.ancestors[1..-1].flat_map { |x| x.instance_methods }.uniq
+
+B.new.call
